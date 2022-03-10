9b7ab7b11a42baff58aa19a88a9764df00fc6932
Eu-Bin KIM <tlrndk123@gmail.com>
2022-03-10 11:37:52 +0000

    [#11] looking up the head of the dataframe

--- explore/explore_fetch_literal2entities.py
+++ explore/explore_fetch_literal2entities.py
@@ -3,8 +3,11 @@ from idiomify.fetchers import fetch_literal2entities
 
 def main():
     train_df, test_df = fetch_literal2entities("d-1-4")
+    # the size
     print(train_df.size)  # 12408 rows
     print(test_df.size)  # 3102 rows
+    # some entries
+    print(train_df.head())
 
 
 if __name__ == '__main__':
